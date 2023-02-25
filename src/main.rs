6b50e09f516fcbe92e72f79a2cdda5d8ca80dca5
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-02-24 21:08:17 -0500

    FIRST COMMIT

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+/* fn main(){
+    let nombre:&str="Catherine";
+    let mut edad:u8 = 1;
+
+    edad+=1;
+
+    println!("My name is {} and I have {} year old",nombre,edad)
+}
+ */
+
+fn main(){
+    let mintemp:i8 = -19;
+    let maxtemp:u8 = 25;
+
+    println!("la temperatura minima en lima es {mintemp} y la maxima temperatura registrada hasta el momento es {maxtemp}");
+}
+

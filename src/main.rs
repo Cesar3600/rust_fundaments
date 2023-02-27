854fc8c9f3540571893f4307da6023c284bf1f16
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-02-27 13:09:00 -0500

    ciclo loop

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 
 fn main(){
-    println!("Selecciona pastilla Roja u Azul");
-    let mut pill:String = String::new();
-    std::io::stdin().read_line(&mut pill).unwrap();
-    pill = pill.trim().to_string();
+    let numero_1:String = "123".to_string();
+    let numero_2:String = "321".to_string();
 
-    if pill == "Roja" {
-        println!("Con la pastilla {} Sales de la Matrix", pill);
-    } 
-    else if pill == "Azul" {
-        println!("Con la pastilla {} Sigues viviendo una fantasia",pill)
-    }else {
-        println!("Tomaste cualquier cosa .. se acaba el mundo.. la jodiste toda.")
+
+   let num1:i32 = numero_1.trim().parse().unwrap();
+   let num2:i32  = numero_2.trim().parse().unwrap();
+
+   let suma = num1 + num2;
+
+    // mostrar los dos numeros en pantalla
+
+    loop{
+        println!("por favor escribir la suma de {} y {}",numero_1,numero_2); 
+    
+        let mut resultado:String = String::new();
+        std::io::stdin().read_line(&mut resultado).unwrap();
+        let resultado_int:i32 = resultado.trim().parse().unwrap();
+    
+        if resultado_int == suma {
+            println!("el resultado: {} es correcto",resultado_int);
+            break;
+        }else{
+            println!("El resultado {} no es correcto",resultado_int)
+        }
     }
-
 }
-

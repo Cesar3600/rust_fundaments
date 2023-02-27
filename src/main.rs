eaa620ea09b6c69de8954e735895f2aa6d44573b
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-02-26 23:44:56 -0500

    clase recibiendo datos de usuarios

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-/* fn main(){
-    let nombre:&str="Catherine";
-    let mut edad:u8 = 1;
-
-    edad+=1;
-
-    println!("My name is {} and I have {} year old",nombre,edad)
-}
- */
 
 fn main(){
-    let mintemp:i8 = -19;
-    let maxtemp:u8 = 25;
+    println!("ingresa tu nombre:");
+    let mut nombre:String = String::new();
+    std::io::stdin().read_line(&mut nombre).unwrap();
+    nombre = nombre.trim().to_string();
 
-    println!("la temperatura minima en lima es {mintemp} y la maxima temperatura registrada hasta el momento es {maxtemp}");
+    println!("ingresa tu edad:");
+    let mut edad:String = String::new();
+    edad = edad.trim().to_string();
+    std::io::stdin().read_line(&mut edad).unwrap();
+
+    let edad_int:u8 = edad.trim().parse().unwrap();
+    
+    println!("Buenos dias {}, ya tienes {} de edad",nombre,edad_int);
+    
+
 }
 

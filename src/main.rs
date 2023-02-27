ab2735a12b00dc6b68e778c6e91ddc1d28797c03
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-02-27 00:55:41 -0500

    Agregado reto de la clasae condicionales

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,18 @@
 
 fn main(){
-    println!("ingresa tu nombre:");
-    let mut nombre:String = String::new();
-    std::io::stdin().read_line(&mut nombre).unwrap();
-    nombre = nombre.trim().to_string();
+    println!("Selecciona pastilla Roja u Azul");
+    let mut pill:String = String::new();
+    std::io::stdin().read_line(&mut pill).unwrap();
+    pill = pill.trim().to_string();
 
-    println!("ingresa tu edad:");
-    let mut edad:String = String::new();
-    edad = edad.trim().to_string();
-    std::io::stdin().read_line(&mut edad).unwrap();
-
-    let edad_int:u8 = edad.trim().parse().unwrap();
-    
-    if edad_int >= 18 && edad_int != 30 {
-        println!("{}, tienes {} puedes ingresar a la discoteca", nombre, edad_int)
-    } else if edad_int < 18 {
-        println!("Aun eres menor de edad, no puedes ingresar")
-    }else if edad_int ==30 {
-        println!("no me caen los de 30, no entras!! ")
+    if pill == "Roja" {
+        println!("Con la pastilla {} Sales de la Matrix", pill);
+    } 
+    else if pill == "Azul" {
+        println!("Con la pastilla {} Sigues viviendo una fantasia",pill)
+    }else {
+        println!("Tomaste cualquier cosa .. se acaba el mundo.. la jodiste toda.")
     }
-
-    //println!("Buenos dias {}, ya tienes {} de edad",nombre,edad_int);
-    
 
 }
 
